46aaa88540b39faf47421c13fa236050d7510fd0
Will Gu <willgu29@gmail.com>
2015-10-06 15:15:40 -0700

    Pre express routing

---
+++ ReactViews/build/.module-cache/936325cdc1c9f26e7815026ebc1db8024cd36c3f.js
@@ -0,0 +1,109 @@
+var Message = React.createClass({displayName: "Message",
+	render: function() {
+
+		return(
+			React.createElement("div", {className: "message"}, 
+       			React.createElement("p", null, React.createElement("em", null, this.props.author), ": ", this.props.children)
+     		)
+		);	
+	}
+
+})
+var MessageBox = React.createClass({displayName: "MessageBox",
+	loadMessagesFromServer: function() {
+		console.log("URL:"+ this.props.url);
+		$.ajax({
+      		url: this.props.url,
+      		dataType: 'json',
+      		cache: false,
+      		success: function(data) {
+        		this.setState({data: data});
+      		}.bind(this),
+      		error: function(xhr, status, err) {
+       			console.error(this.props.url, status, err.toString());
+      		}.bind(this)
+    	});
+	},
+	handleMessageSubmit: function(comment) {
+		//Instant comment load... don't need this for now
+		// var comments = this.state.data;
+  //   	var newComments = comments.concat([comment]);
+  //   	this.setState({data: newComments});
+    	$.ajax({
+      		url: this.props.url,
+      		dataType: 'json',
+      		type: 'POST',
+      		data: comment,
+      		success: function(data) {
+      			this.loadMessagesFromServer();
+        		// this.setState({data: data});
+      		}.bind(this),
+      		error: function(xhr, status, err) {
+        		console.error(this.props.url, status, err.toString());
+      		}.bind(this)
+    	});
+  	},
+	getInitialState: function() {
+		return ({data: []});
+	},
+	componentDidMount: function() {
+		this.loadMessagesFromServer();
+		//Don't need constant polling for now
+    	//setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+	},
+	render: function() {
+		return(
+			React.createElement("div", null, 
+				React.createElement("h4", null, "Comments"), 
+				React.createElement(MessageList, {data: this.state.data}), 
+				React.createElement(MessageForm, {onCommentSubmit: this.handleCommentSubmit})
+			)
+		);
+	}
+
+});
+
+var MessageList = React.createClass({displayName: "MessageList",
+
+	render: function() {
+		var messageNodes = this.props.data.map(function (message) {
+      		return (
+        	React.createElement(Message, {author: message.authorFullName}, 
+          		message.text
+        	)
+      		);
+    	});
+    	return (
+      		React.createElement("div", {className: "messageList"}, 
+        	messageNodes
+      		)
+    	);
+	}
+});
+
+var MessageForm = React.createClass({displayName: "MessageForm",
+	handleSubmit: function(e) {
+  	  	e.preventDefault();
+
+    	var text = React.findDOMNode(this.refs.text).value.trim();
+    	if (!text) {
+      		return;
+    	}
+    	React.findDOMNode(this.refs.text).value = '';
+    	return;
+  },
+	render: function() {
+		return(
+			React.createElement("form", {className: "messageForm", onSubmit: this.handleSubmit}, 
+        		React.createElement("input", {type: "text", size: "60", placeholder: "Say something positive/negative...", ref: "text"}), 
+        		React.createElement("input", {type: "submit", value: "Post"})
+      		)
+		);
+	}
+
+});
+
+var href=  window.location.href;
+var user_id = href.substr(href.lastIndexOf('/') + 1);
+var urlCall = "/api/message/"+user_id;
+React.render(React.createElement(MessageBox, {url: urlCall}), document.getElementById("chatroom"));
