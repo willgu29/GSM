25c21982025ea31e774e72060e1c7166e7b4a709
Will Gu <willgu29@gmail.com>
2015-10-17 20:42:07 -0700

    Install history and react router

---
+++ ReactViews/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { Router, Route, Link } from 'react-router'
+
+const App = React.createClass({
+	render: function() {
+		return(
+			<p>Hello world!</p>
+		);
+	}
+});
+
+React.render((<Router>
+				<Route path="/" component={App}>
+				<Route />
+			</Router>
+			), document.body);
